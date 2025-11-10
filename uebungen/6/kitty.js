c85bdad18aef147041d6d39485d297f3eb475ccb
Franca Lenz <franca.lenz@stud.htwk-leipzig.de>
2025-11-10 15:42:34 +0100

    refactor: changes the whole page to be more modern

--- uebungen/6/kitty.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("kitty").addEventListener("click", function() {
-    var bild = document.getElementById("bild");
-    if (bild.style.display === "none") {
-        bild.style.display = "block";
-    } else {
-        bild.style.display = "none";
-    }
-});
